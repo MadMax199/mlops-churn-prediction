383a932886730a4a18e56631a9f76c42f5962143
MadMax199 <maxkrampert@web.de>
2026-08-25 21:07:47 +0200

    start mit pipline

--- src/utils.py
+++ src/utils.py
@@ -58,3 +58,51 @@ def get_missing_values(df: DataFrame) -> DataFrame:
             F.asc("column"),
         )
     )
+
+
+def clean_user(df:DataFrame) -> DataFrame:
+    """Bereinigt den User DataFram nacheem es aus der csv gelesen wurde für weitere Verarbeitungsschritte"""
+    return(df.select(
+            F.col("id").alias("user_id"),
+            F.sha1("email").alias("email"),
+            F.to_timestamp(
+                "creation_date",
+                "MM-dd-yyyy HH:mm:ss",
+            ).alias("creation_date"),
+            F.to_timestamp(
+                "last_activity_date",
+                "MM-dd-yyyy HH:mm:ss",
+            ).alias("last_activity_date"),
+            F.initcap("firstname").alias("firstname"),
+            F.initcap("lastname").alias("lastname"),
+            "address",
+            "canal",
+            "country",
+            F.col("gender").cast("int").alias("gender"),
+            F.col("age_group").cast("int").alias("age_group"),
+            F.col("churn").cast("int").alias("churn"))
+)
+
+def clean_orders(df:DataFrame) -> DataFrame:
+    """Bereinigt den Orders DataFrame nacheem es aus der csv gelesen wurde für weitere Verarbeitungsschritte"""
+    return(df.select(
+                    F.col("amount").cast("int").alias("amount"),
+                    F.col("id").alias("order_id"),
+                    "user_id",
+                    F.col("item_count").cast("int").alias("item_count"),
+                    F.to_timestamp(
+                        "transaction_date",
+                        "MM-dd-yyyy HH:mm:ss",
+                    ).alias("creation_date"),
+                )
+                )
+def aggregate_orders(df:DataFrame) -> DataFrame:
+    """Aggregiert den Orders DataFrame nach user_id und berechnet die Summe der amount und item_count Spalten"""
+    return(df.groupBy("user_id").agg(
+        F.count("*").alias("order_count"),
+        F.sum("amount").alias("total_amount"),
+        F.sum("item_count").alias("total_item"),
+        F.max("creation_date").alias("last_transaction"))       
+        )
+
+def aggre
